1b9d102eaeacac47712284ee19dcaaef47945651
ketchanji <valerydjanya8@gmail.com>
2023-01-25 14:02:51 +0100

    UI updates

--- src/components/UI/ProductCard.jsx
+++ src/components/UI/ProductCard.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
+import { useNavigate } from "react-router-dom";
 import { CardInfo } from '../UI/CardInfo';
 export const ProductCard = (props) => {
+
+  const navigate = useNavigate();
+      
+        const navigatetoBook = () => {
+          navigate("/book");
+        };
+
+
     return (
         <div class="lg:w-1/3 md:w-1/4 p-6 w-full shadow-lg shadow-slate-300">
             <a class="block relative h-48 rounded overflow-hidden">
@@ -60,6 +69,7 @@ export const ProductCard = (props) => {
             </div>
             <button
               type="button"
+              onClick={navigatetoBook}
               class="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">
               Book Now
             </button>
